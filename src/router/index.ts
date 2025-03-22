1a3895489befe2486dc8fe4e185e944cc042fff3
Julio César Gama <me@juliocesargama.tech>
2025-03-22 02:48:08 -0300

    addig main page, campaign and neighborhood major operations

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import NeighborhoodView from "../views/NeighborhoodView.vue";
+import NeighborhoodCreateView from "./../views/inputs/NeighborhoodCreateView.vue";
+import NeighborhoodEditView from "@/views/inputs/NeighborhoodEditView.vue";
+import CampaignView from "@/views/CampaignView.vue";
+import CampaignCreateView from "@/views/inputs/CampaignCreateView.vue";
+import CampaignEditView from "@/views/inputs/CampaignEditView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +14,36 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/campaign",
+      name: "campaign",
+      component: CampaignView
+    },
+    {
+        path: "/campaign/create",
+        name: "campaign-create",
+        component: CampaignCreateView
+    },
+    {
+        path: "/campaign/:id/edit",
+        name: "campaign-edit",
+        component: CampaignEditView
+    },
+    {
+      path: "/neighborhood",
+      name: "neighborhood",
+      component: NeighborhoodView,
+    },
+    {
+        path: "/neighborhood/create",
+        name: "neighborhood-create",
+        component: NeighborhoodCreateView,
+    },
+    {
+        path: "/neighborhood/:id/edit",
+        name: "neighborhood-edit",
+        component: NeighborhoodEditView,
     },
     {
       path: "/about",
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container mt-5">
+      <div class="card">
+          <div class="card-header">
+              <h3>Sobre</h3>
+          </div>
+          <div class="card-body text-center m-5">
+    
+              <b>Versão 1.0</b>
+          </div>
+      </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
