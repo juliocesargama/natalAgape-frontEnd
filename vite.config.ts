010dbbf1e97265cd3dfc7ca00cedc15c8bc21bd5
Julio César Gama <me@juliocesargama.tech>
2025-03-26 23:46:41 -0300

    fix for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,33 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: loadEnv(process.env.NODE_ENV || 'development', process.cwd()).VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = `${env.VITE_API_URL}`;
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: API_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
+
+
 
 
