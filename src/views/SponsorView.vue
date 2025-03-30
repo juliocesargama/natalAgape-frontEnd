91ea29e382862a38065cc312dcee11914473bc25
Julio César Gama <me@juliocesargama.tech>
2025-03-30 20:04:05 -0300

    sponsor views

---
+++ src/views/SponsorView.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Doadores
+                    <RouterLink to="/sponsor/create" class="btn btn-primary float-end" aria-describedby="Botão para incluir novo doador">Novo Doador</RouterLink>
+                </h3>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th><b>Nome</b></th>
+                            <th><b>Telefone</b></th>
+                            <th><b>Ações</b></th>
+                        </tr>
+                    </thead>
+                    <tbody v-if="sponsors.length > 0">
+                        <tr v-for="sponsor in sponsors" :key="sponsor.sponsorId">
+                            <td>{{ sponsor.sponsorName }}</td>
+                            <td>{{ sponsor.sponsorPhone }}</td>
+                            <td><RouterLink :to="{path: '/sponsor/'+sponsor.sponsorId +'/edit'}" class="btn btn-primary" aria-describedby="Botão para alterar o doador">Alterar</RouterLink></td>
+                        </tr>
+                    </tbody>
+                    <tbody v-else>
+                        <tr>
+                            <td colspan="4">Carregando Doadores...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+import { type sponsor } from "../models/sponsor";
+
+export default {
+
+    name: 'sponsor-view',
+    data() {
+        return {
+            sponsors: [] as sponsor[]
+        }
+    },
+    mounted() {
+        this.getsponsors();
+    },
+    methods: {
+        getsponsors() {
+            var url = 'api/sponsor';
+            return axios.get(url).then(result => this.sponsors = result.data);
+        }
+    },
+}
+</script>
+
