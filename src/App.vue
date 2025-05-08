64493b8b52e7b5ea750e7d31efa988d34936341d
Julio César Gama <me@juliocesargama.tech>
2025-05-07 22:41:42 -0300

    minor fix

--- src/App.vue
+++ src/App.vue
@@ -102,10 +102,6 @@ const logout = () => {
           <RouterLink class="navbar-brand" to="/" aria-current="Página Inicial"><b>
               <h2>Ágape</h2>
             </b></RouterLink>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
         </div>
         </nav>
     </div>
