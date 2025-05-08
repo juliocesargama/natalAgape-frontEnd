a68ef5e98c954861bef5af92ca81db8c46c67cf6
Julio César Gama <me@juliocesargama.tech>
2025-05-07 22:58:16 -0300

    vercel build fix atempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
+import { build, defineConfig, loadEnv, Rollup } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => {
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
  const env = loadEnv(mode, process.cwd());
 
   const API_URL = `${env.VITE_API_URL}`;
+
 
   return {
     server: {
@@ -27,7 +28,14 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
+    },
+    build: {
+      rollupOptions: {
+        external: [
+          'jwt-decode',
+        ]
     }
+    },
   }
 })
 
