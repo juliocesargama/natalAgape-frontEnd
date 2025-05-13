a53a9d4ef6dce68d3f44e658995c68015fd3cb4c
Julio César Gama <me@juliocesargama.tech>
2025-05-12 21:09:44 -0300

    approach 3

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,4 @@
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from "jwt-decode"; // Importa a biblioteca jwt-decode
 
 export function getUserRole() {
   const token = localStorage.getItem("jwtToken");
